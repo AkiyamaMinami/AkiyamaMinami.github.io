29088b8648a509232274dd01b3b1d5d8a972749e
mobs <zsqmobs@gmail.com>
2022-08-03 15:44:04 +0800

    update master

--- docs/.vuepress/components/About.vue
+++ docs/.vuepress/components/About.vue
@@ -1,7 +1,42 @@
 <template>
-  <div>吸铁石睡觉</div>
+  <div class="about-container">
+    <div class="dsm-container">
+      <div class="tip-container">
+        <div class="tip-item">哎？吸铁石睡觉？</div>
+        <div class="tip-item">李在赣神魔？</div>
+      </div>
+      <div>
+        <img
+          class="dsm-img"
+          src="https://s2.loli.net/2022/08/02/sKdIG7aU8uegpb6.jpg"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=ZCOOL+KuaiLe&display=swap");
+</style>
+<style scoped>
+.dsm-container {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+.tip-container {
+  font-family: "ZCOOL KuaiLe", cursive;
+  font-size: 40px;
+}
+.tip-item {
+  margin-bottom: 20px;
+}
+.dsm-img {
+  margin-top: 15px;
+  width: 280px;
+  height: 170px;
+}
+</style>
